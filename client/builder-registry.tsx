5b6f2dc86e45cf4cacaef312223e1c512458e828
Rishab Chaudhary <rishab@ASBLL00134.local>
2026-02-05 20:22:24 +0530

    change the api key

--- client/builder-registry.tsx
+++ client/builder-registry.tsx
@@ -1,14 +1,18 @@
 import { Builder } from "@builder.io/react";
+import { builder } from "@builder.io/sdk";
 import PropertyRates from "./components/PropertyRates";
 import { Card } from "./components/ui/card";
 import { Button } from "./components/ui/button";
+
+// Initialize Builder SDK with your Public API Key
+builder.init(import.meta.env.VITE_PUBLIC_BUILDER_KEY);
 
 // Register PropertyRates as a Builder component
 Builder.registerComponent(PropertyRates, {
   name: "PropertyRates",
   inputs: [],
   image:
-    "https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F6bef27ee40d24f3b88239fd7e616f82a",
+    "https://cdn.builder.io/api/v1/image/assets%2F770b5ae7fc584faebcf10a33186df6f8%2F25396a48c928458e8936faafe8307f3b",
   defaultStyles: {
     width: "100%",
   },
@@ -29,3 +33,4 @@ Builder.registerComponent(Button, {
 });
 
 export {};
+
